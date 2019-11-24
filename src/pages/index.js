6f545f2807fb42b22fd0604ed4184cf6e1390397
Ankit <salian.ankit@tcs.com>
2019-11-24 21:09:19 +0530

    Added styling for blog page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,18 @@
 import React from "react"
-import { graphql ,Link } from "gatsby"
+import { graphql ,Link } from "gatsby";
+import styled from "styled-components";
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`
+
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  color: blue;
+`
 
 export default ({data}) => {
   console.log(data);
@@ -16,7 +26,9 @@ export default ({data}) => {
       {
         data.allMarkdownRemark.edges.map(({node}) => (
           <div key={node.id}>
-            <span>{node.frontmatter.title} - {node.frontmatter.date}</span>
+            <BlogLink to={node.fields.slug} >
+              <BlogTitle>{node.frontmatter.title} - {node.frontmatter.date}</BlogTitle>
+            </BlogLink>
             <p>{node.excerpt}</p>
           </div>
         ))
@@ -27,7 +39,7 @@ export default ({data}) => {
 
 export const query = graphql`
   query {
-    allMarkdownRemark {
+    allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
       edges {
         node {
           id
@@ -35,6 +47,9 @@ export const query = graphql`
             date
             description
             title
+          }
+          fields {
+            slug
           }
           excerpt
         }
